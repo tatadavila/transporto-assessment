fe6d298e1a2bd4bdf50a69bb5f40c7fe06725edf
tats-devteam <tatianad@eliteautomationgroup.com>
2023-05-25 18:57:55 -0500

    modify header component in order to remove unnecesary items

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,33 +1,20 @@
 // @vendors
-import { Container, Nav, NavDropdown, Navbar } from "react-bootstrap";
+import { Container, Nav, Navbar } from "react-bootstrap";
 
 export const Header = () => {
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+      <Container
+        className="m-0 py-0 pl-10 pr-0 d-flex justify-content-between align-items-center"
+        fluid
+      >
+        <Navbar.Brand className="" href="#home">
+          Task Manarger
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-          <Nav>
-            <Nav.Link href="#deets">More deets</Nav.Link>
-            <Nav.Link eventKey={2} href="#memes">
-              Dank memes
-            </Nav.Link>
+          <Nav className="mx-auto">
+            <Nav.Link>Home</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
