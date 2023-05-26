16195df9d37a30a7546a74276f6b6cc17ffae49e
tats-devteam <tatianad@eliteautomationgroup.com>
2023-05-26 05:46:46 -0500

    implement TaskCard component

--- src/components/cardgrid/CardGrid.jsx
+++ src/components/cardgrid/CardGrid.jsx
@@ -1,3 +1,30 @@
+// @vendors
+import { Col, Container, Row } from "react-bootstrap";
+import { useSelector } from "react-redux";
+
+// @components
+import { TaskCard } from "../taskcard";
+
+import "./cardGrid.css";
+
 export const CardGrid = () => {
-  return <div>CardGrid</div>;
+  const tasks = useSelector((state) => state.data.tasks);
+
+  console.log("TAKS", tasks);
+
+  return (
+    <Container>
+      <Row xs={1} md={2} className="g-5 mt-0">
+        {tasks.map((task) => {
+          return (
+            !task.done && (
+              <Col key={task.id}>
+                <TaskCard taskInfo={task} />
+              </Col>
+            )
+          );
+        })}
+      </Row>
+    </Container>
+  );
 };
