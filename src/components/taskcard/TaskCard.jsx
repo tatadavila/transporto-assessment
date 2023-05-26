c1c71e1fef6c6101bd2c0f7f3678ae373bf065db
tats-devteam <tatianad@eliteautomationgroup.com>
2023-05-26 05:47:03 -0500

    modify component

--- src/components/taskcard/TaskCard.jsx
+++ src/components/taskcard/TaskCard.jsx
@@ -1,3 +1,35 @@
-export const TaskCard = () => {
-  return <div>Card</div>;
+// @vendors
+import { Button, Card } from "react-bootstrap";
+
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+
+// @styles
+import "./taskCard.css";
+
+export const TaskCard = ({ taskInfo }) => {
+  return (
+    <Card>
+      <Card.Header as="h6">TAREA #{taskInfo?.id}</Card.Header>
+      <Card.Body>
+        {taskInfo.subTasks.length === 0 ? (
+          <Link to={`/task/${taskInfo.id}`}>
+            <Button id={taskInfo.id}>Hacer Tarea #{taskInfo.id}</Button>
+          </Link>
+        ) : (
+          taskInfo.subTasks.map((subTask) => {
+            return (
+              <Link key={subTask.id} to={`/task/${subTask.id}`}>
+                <Button>Hacer Tarea #{subTask.id}</Button>
+              </Link>
+            );
+          })
+        )}
+      </Card.Body>
+    </Card>
+  );
+};
+
+TaskCard.propTypes = {
+  taskInfo: PropTypes.object.isRequired,
 };
