4917804ac0bc6808319408a684afedaaaf1e3d59
tats-devteam <tatianad@eliteautomationgroup.com>
2023-05-26 18:06:38 -0500

    Implement PendingTask into Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,12 @@
 // @components
 import { CardGrid } from "../components";
+import { PendingTask } from "../components/modals";
 
 export const Home = () => {
-  return <CardGrid />;
+  return (
+    <>
+      <CardGrid />
+      <PendingTask />
+    </>
+  );
 };
