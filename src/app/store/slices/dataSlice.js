5a790828766f0f237abaccc47e745736ddf92961
tats-devteam <tatianad@eliteautomationgroup.com>
2023-05-26 17:56:10 -0500

    import helper functions, modify markTaskDone action and crete new one

--- src/app/store/slices/dataSlice.js
+++ src/app/store/slices/dataSlice.js
@@ -1,6 +1,13 @@
 // @vendors
 import { createSlice } from "@reduxjs/toolkit";
 
+// @helpers
+import {
+  findTaskIndexById,
+  isRequiredTaskDone,
+  searchTask,
+} from "../../../helpers";
+
 const initialState = {
   tasks: [
     { done: false, id: 1, required: true, subTasks: [] },
@@ -35,64 +42,33 @@ const initialState = {
     },
     { done: false, id: 6, required: false, subTasks: [] },
   ],
+  showPendingTaskModal: false,
+  requiredTaskId: 0,
 };
-
-const isRequiredDone = (task, state) => {
-  if (task.required && !task.done) {
-    return false;
-  }
-
-  if (task.subTasks && task.subTasks.length > 0) {
-    return task.subTasks.every(
-      (subTask) => subTask.done && isRequiredDone(subTask, state),
-    );
-  }
-
-  return true;
-};
-
-//const allSubTasksDone = (subTasks) => subTasks.every((subTask) => subTask.done);
 
 export const dataSlice = createSlice({
   name: "data",
   initialState,
   reducers: {
-    updateTasks: (state, action) => {
-      console.log("PAYLOAD", action.payload);
+    markTaskDone: (state, action) => {
       const taskId = Number(action.payload);
+      const indexOfTask = findTaskIndexById(taskId, state.tasks);
+      const task = searchTask(taskId, state.tasks);
+      const isReqDone = isRequiredTaskDone(indexOfTask, taskId, state.tasks);
 
-      const task = state.tasks.find((task) => task.id === taskId);
-
-      if (task) {
-        // Check if the task is required and not done
-        if (task === state.tasks[0] && task.required && !task.done) {
-          task.done = true;
-          console.log(`Task #${task.id} completed.`);
-        } else if (isRequiredDone(state.tasks[0])) {
-          task.done = true;
-          console.log(`Task #${task.id} completed.`);
-        } else {
-          // Find the parent task if it exists
-          const parentTask = state.tasks.find((parent) =>
-            parent.subTasks.some((subTask) => subTask.id === taskId),
-          );
-
-          if (parentTask && !parentTask.done) {
-            console.log(
-              `You must finish Task #${parentTask.id} first before proceeding to its sub-tasks.`,
-            );
-          } else {
-            task.done = true;
-            console.log(`Task #${task.id} completed.`);
-          }
-        }
+      if (isReqDone.value) {
+        task.done = true;
       } else {
-        console.log(`Task with id ${taskId} not found.`);
+        state.requiredTaskId = isReqDone?.taskId;
+        state.showPendingTaskModal = true;
       }
+    },
+    setShowPendindTaskModal: (state, action) => {
+      state.showPendingTaskModal = action.payload;
     },
   },
 });
 
-export const { updateTasks } = dataSlice.actions;
+export const { markTaskDone, setShowPendindTaskModal } = dataSlice.actions;
 
 export default dataSlice.reducer;
